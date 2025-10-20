b511a001327ef17370d7db4c45c27d2055d13ba1
Andy Coloma <andy.coloma@inacapmail.cl>
2025-10-20 16:51:42 -0300

    feat: preparación prueba

--- Sistema_Visitas/Visitas/admin.py
+++ Sistema_Visitas/Visitas/admin.py
@@ -4,4 +4,17 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Visita
 
-admin.site.register(Visita)
+#admin.site.register(Visita)
+
+@admin.register(Visita)
+class VisitasAdmin(admin.ModelAdmin):
+    list_display = ('nombre','rut','fecha','motivo_visita')
+    list_filter = ('fecha',)
+    search_fields = ('nombre','rut')
+    list_editable = ('motivo_visita',)
+    ordering = ('fecha',)
+    readonly_fields = ('hora_entrada','hora_salida')
+
+    fieldsets = (
+        ('Persona', {'fields': ('nombre', 'rut')})
+                ,)
